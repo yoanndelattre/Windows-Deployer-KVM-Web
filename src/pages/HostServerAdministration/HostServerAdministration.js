c43b9e79c0b6a601a0357c45b112f2cf109531a9
yoanndelattre <yoanndelattre21@gmail.com>
2021-04-25 12:43:56 +0200

    add stop and delete action

--- src/pages/HostServerAdministration/HostServerAdministration.js
+++ src/pages/HostServerAdministration/HostServerAdministration.js
@@ -9,8 +9,10 @@ export default class HostServerAdministration extends Component {
     state = {
         getWindowsDeployerServerName: '',
         getWindowsDeployerServerStatus: '',
+        getWindowsDeployerServerIp: 'unknown',
         getWindowsDeployerServerID: '',
-        getInfos: ''
+        getInfos: 'unknown',
+        getWindowsDeployerVolumeID: ''
     }
 
     componentDidMount() {
@@ -48,6 +50,24 @@ export default class HostServerAdministration extends Component {
         })
         .then((res) => {
             this.setState({getWindowsDeployerServerStatus: res.data.servers.[0].state})
+            this.getWindowsDeployerServerIp()
+        })
+        .catch((err) => {
+            console.error(err)
+            this.setState({getWindowsDeployerServerStatus: ''})
+            this.getWindowsDeployerServerIp()
+        })
+    }
+
+    getWindowsDeployerServerIp = () => {
+        axios.get('https://api.scaleway.com/instance/v1/zones/fr-par-1/servers', {
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Auth-Token': localStorage.getItem('Secret_key')
+            }
+        })
+        .then((res) => {
+            this.setState({getWindowsDeployerServerStatus: res.data.servers.[0].public_ip.address})
             this.getInfos()
         })
         .catch((err) => {
@@ -76,12 +96,23 @@ export default class HostServerAdministration extends Component {
         }, 2500);
         setTimeout(() => {
             this.enableCloudInit()
-        }, 3000);
+        }, 3500);
+        setTimeout(() => {
+            this.startWindowsDeployerServer()
+        }, 4500);
+        setTimeout(() => {
+            new Noty({
+                text: 'Installation takes an average of 4 minutes. Please do not stop Windows-Deployer Vm.',
+                theme: 'bootstrap-v4',
+                type: 'information',
+                layout: 'bottomCenter',
+            }).show();
+        }, 5200);
     }
 
     createWindowsDeployerServer = () => {
         if (this.state.getWindowsDeployerServerName !== 'Windows-Deployer') {
-            var data = {"name": "Windows-Deployer", "commercial_type": "GP1-S", "project": localStorage.getItem('Project_ID'), "image": "16152446-99ed-4795-9d3f-87ec2f5b891d"}
+            var data = {"name": "Windows-Deployer", "commercial_type": "GP1-S", "project": localStorage.getItem('Project_ID'), "image": "16152446-99ed-4795-9d3f-87ec2f5b891d", "enable_ipv6": true}
             axios.post('https://api.scaleway.com/instance/v1/zones/fr-par-1/servers', data, {
                 headers: {
                     'Content-Type': 'application/json',
@@ -146,7 +177,7 @@ export default class HostServerAdministration extends Component {
         }
         else {
             new Noty({
-                text: 'Error',
+                text: 'The Windows-Deployer Vm does not exist',
                 theme: 'bootstrap-v4',
                 type: 'error',
                 layout: 'bottomCenter',
@@ -184,12 +215,253 @@ export default class HostServerAdministration extends Component {
         }
         else {
             new Noty({
-                text: 'Error',
+                text: 'The Windows-Deployer Vm does not exist',
                 theme: 'bootstrap-v4',
                 type: 'error',
                 layout: 'bottomCenter',
             }).show();
         }
+    }
+
+    startWindowsDeployerServer = () => {
+        if (this.state.getWindowsDeployerServerName === 'Windows-Deployer') {
+            if (this.state.getWindowsDeployerServerStatus === 'stopped') {
+                var data = {"action": "poweron"}
+                axios.post('https://api.scaleway.com/instance/v1/zones/fr-par-1/servers/' + this.state.getWindowsDeployerServerID + '/action', data, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-Auth-Token': localStorage.getItem('Secret_key')
+                    }
+                })
+                .then((res) => {
+                    console.log(res)
+                    new Noty({
+                        text: 'The Windows-Deployer Vm has been started',
+                        theme: 'bootstrap-v4',
+                        type: 'success',
+                        layout: 'bottomCenter',
+                    }).show();
+                })
+                .catch((err) => {
+                    console.error(err)
+                    new Noty({
+                        text: 'Error',
+                        theme: 'bootstrap-v4',
+                        type: 'error',
+                        layout: 'bottomCenter',
+                    }).show();
+                })
+            }
+            else {
+                new Noty({
+                    text: 'The Windows-Deployer Vm is in an unstable state or is already started',
+                    theme: 'bootstrap-v4',
+                    type: 'error',
+                    layout: 'bottomCenter',
+                }).show();
+            }
+        }
+        else {
+            new Noty({
+                text: 'The Windows-Deployer Vm does not exist',
+                theme: 'bootstrap-v4',
+                type: 'error',
+                layout: 'bottomCenter',
+            }).show();
+        }
+    }
+
+    startingWindowsDeployerServer = () => {
+        this.getWindowsDeployerServerName()
+        setTimeout(() => {
+            this.getWindowsDeployerServerID()
+        }, 500);
+        setTimeout(() => {
+            this.startWindowsDeployerServer()
+        }, 1500);
+    }
+
+    stopWindowsDeployerServer = () => {
+        if (this.state.getWindowsDeployerServerName === 'Windows-Deployer') {
+            if (this.state.getWindowsDeployerServerStatus === 'running') {
+                var data = {"action": "poweroff"}
+                axios.post('https://api.scaleway.com/instance/v1/zones/fr-par-1/servers/' + this.state.getWindowsDeployerServerID + '/action', data, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-Auth-Token': localStorage.getItem('Secret_key')
+                    }
+                })
+                .then((res) => {
+                    console.log(res)
+                    new Noty({
+                        text: 'The Windows-Deployer Vm is shutting down',
+                        theme: 'bootstrap-v4',
+                        type: 'success',
+                        layout: 'bottomCenter',
+                    }).show();
+                })
+                .catch((err) => {
+                    console.error(err)
+                    new Noty({
+                        text: 'Error',
+                        theme: 'bootstrap-v4',
+                        type: 'error',
+                        layout: 'bottomCenter',
+                    }).show();
+                })
+            }
+            else {
+                new Noty({
+                    text: 'The Windows-Deployer Vm is in an unstable state or is already stopped',
+                    theme: 'bootstrap-v4',
+                    type: 'error',
+                    layout: 'bottomCenter',
+                }).show();
+            }
+        }
+        else {
+            new Noty({
+                text: 'The Windows-Deployer Vm does not exist',
+                theme: 'bootstrap-v4',
+                type: 'error',
+                layout: 'bottomCenter',
+            }).show();
+        }
+    }
+
+    stoppingWindowsDeployerServer = () => {
+        this.getWindowsDeployerServerName()
+        setTimeout(() => {
+            this.getWindowsDeployerServerID()
+        }, 500);
+        setTimeout(() => {
+            this.stopWindowsDeployerServer()
+        }, 1500);
+    }
+
+    deleteWindowsDeployerServer = () => {
+        if (this.state.getWindowsDeployerServerName === 'Windows-Deployer') {
+            if (this.state.getWindowsDeployerServerStatus === 'stopped') {
+                axios.delete('https://api.scaleway.com/instance/v1/zones/fr-par-1/servers/' + this.state.getWindowsDeployerServerID, {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-Auth-Token': localStorage.getItem('Secret_key')
+                    }
+                })
+                .then((res) => {
+                    console.log(res)
+                    new Noty({
+                        text: 'The Windows-Deployer Vm is in the process of being removed',
+                        theme: 'bootstrap-v4',
+                        type: 'success',
+                        layout: 'bottomCenter',
+                    }).show();
+                })
+                .catch((err) => {
+                    console.error(err)
+                    new Noty({
+                        text: 'Error',
+                        theme: 'bootstrap-v4',
+                        type: 'error',
+                        layout: 'bottomCenter',
+                    }).show();
+                })
+            }
+            else if (this.state.getWindowsDeployerServerStatus === 'running') {
+                new Noty({
+                    text: 'First stop Windows-Deployer Vm',
+                    theme: 'bootstrap-v4',
+                    type: 'error',
+                    layout: 'bottomCenter',
+                }).show();
+            }
+            else {
+                new Noty({
+                    text: 'The Windows-Deployer Vm is in an unstable state',
+                    theme: 'bootstrap-v4',
+                    type: 'error',
+                    layout: 'bottomCenter',
+                }).show();
+            }
+        }
+        else {
+            new Noty({
+                text: 'The Windows-Deployer Vm does not exist',
+                theme: 'bootstrap-v4',
+                type: 'error',
+                layout: 'bottomCenter',
+            }).show();
+        }
+    }
+
+    getWindowsDeployerVolumeID = () => {
+        axios.get('https://api.scaleway.com/instance/v1/zones/fr-par-1/volumes', {
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Auth-Token': localStorage.getItem('Secret_key')
+            }
+        })
+        .then((res) => {
+            this.setState({getWindowsDeployerVolumeID: res.data.volumes.[0].id})
+            new Noty({
+                text: 'Successful Volume ID retrieval',
+                theme: 'bootstrap-v4',
+                type: 'success',
+                layout: 'bottomCenter',
+            }).show();
+        })
+        .catch((err) => {
+            console.error(err)
+            new Noty({
+                text: 'Get Volume Error',
+                theme: 'bootstrap-v4',
+                type: 'error',
+                layout: 'bottomCenter',
+            }).show();
+        })
+    }
+
+    deleteWindowsDeployerVolume = () => {
+        axios.delete('https://api.scaleway.com/instance/v1/zones/fr-par-1/volumes/' + this.state.getWindowsDeployerVolumeID, {
+            headers: {
+                'Content-Type': 'application/json',
+                'X-Auth-Token': localStorage.getItem('Secret_key')
+            }
+        })
+        .then((res) => {
+            console.log(res)
+            new Noty({
+                text: 'The Windows-Deployer Volume is in the process of being removed',
+                theme: 'bootstrap-v4',
+                type: 'success',
+                layout: 'bottomCenter',
+            }).show();
+        })
+        .catch((err) => {
+            console.error(err)
+            new Noty({
+                text: 'The volume cannot be deleted',
+                theme: 'bootstrap-v4',
+                type: 'error',
+                layout: 'bottomCenter',
+            }).show();
+        })
+    }
+
+    deletingWindowsDeployerServer = () => {
+        this.getWindowsDeployerServerName()
+        setTimeout(() => {
+            this.getWindowsDeployerServerID()
+        }, 500);
+        setTimeout(() => {
+            this.deleteWindowsDeployerServer()
+        }, 1500);
+        setTimeout(() => {
+            this.getWindowsDeployerVolumeID()
+        }, 3500);
+        setTimeout(() => {
+            this.deleteWindowsDeployerVolume()
+        }, 4500);
     }
    
     render() {
@@ -197,10 +469,21 @@ export default class HostServerAdministration extends Component {
             <Fragment>
                 <div className='vm-status'>
                     <p>Windows-Deployer VM Status: {this.state.getInfos}</p>
-
+                </div>
+                <div className='vm-ip'>
+                    <p>Windows-Deployer VM IP: {localStorage.getItem('getWindowsDeployerServerIp')}</p>
                 </div>
                 <div className='init-vm'>
                     <button onClick={this.initWindowsDeployerServer}>Initialize Windows-Deployer VM</button>
+                </div>
+                <div className='starting-vm'>
+                    <button onClick={this.startingWindowsDeployerServer}>Start Windows-Deployer VM</button>
+                </div>
+                <div className='stopping-vm'>
+                    <button onClick={this.stoppingWindowsDeployerServer}>Stop Windows-Deployer VM</button>
+                </div>
+                <div className='deleting-vm'>
+                    <button onClick={this.deletingWindowsDeployerServer}>Delete Windows-Deployer VM</button>
                 </div>
             </Fragment>
         );
