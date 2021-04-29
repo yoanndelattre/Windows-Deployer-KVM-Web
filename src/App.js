72b04ca649984bb284229b5aec276c39b3cdca9e
yoanndelattre <yoanndelattre21@gmail.com>
2021-04-29 21:02:17 +0200

    add link docs

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
   render() {
     return (
       <Fragment>
+        <a className="docs-link" target="blank" href="https://github.com/yoanndelattre/Windows-Deployer-KVM-App#windows-deployer-kvm-app">Documentation</a>
         <div className="conteneur-flexbox">
           <div className="flexbox-1"><ApiScaleway/></div>
           <div className="flexbox-2"><HostServerAdministration/></div>
